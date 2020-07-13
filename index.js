ed68a3297b4ae678210c3ceec4840a2274bbe9cf
Кащеев Илья <tactical949@gmail.com>
2020-07-13 17:08:51 +0600

    update index.js

--- index.js
+++ index.js
@@ -1,22 +1,31 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const { getTwoArgsOperation } = require("./operations");
+const { getOneArgOperation} = require("./operations");
 
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/", function(request, response) {
+app.post("/two-args", function(request, response) {
     const { firstArg, secondArg, operation } = request.body;
-    if (operation === '+') {
-        response.status(200).json(firstArg + secondArg);
-    } else if (operation === '-') {
-        response.status(200).json(firstArg - secondArg);
-    } else if (operation === '/') {
-        response.status(200).json(firstArg / secondArg);
-    } else if (operation === '*') {
-        response.status(200).json(firstArg * secondArg);
+    try {
+        response
+            .status(200)
+            .json(getTwoArgsOperation(operation)(firstArg, secondArg));
+    } catch (e) {
+        response.status(400).json(e.message);
     }
 });
 
-app.listen(3000, () => {
-    console.log('App started on https://localhost:3000');
-});
+app.post("/one-arg", function(request,response) {
+    const { oneArg, opeation} = request.body;
+    try{
+        response
+            .status(200)
+            .json(getOneArgOperation(opeation)(oneArg));
+    } catch(e){
+        response.status(400).json(e.message);
+    }
+}); 
+
+module.exports = app;
