8d6848fa21bd012a25d3817ac7112ebc19773b2e
Кащеев Илья <tactical949@gmail.com>
2020-07-22 01:14:37 +0600

    add front

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const path = require('path');
 const bodyParser = require('body-parser');
 const { getTwoArgsOperation } = require("./operation");
 const { getOneArgOperation} = require("./operation");
 
 const app = express();
 app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+    response.sendFile(path.join(__dirname + '/index.html'));
+})
 
 app.post("/two-args", (request, response) => {
     const { firstArg, secondArg, operation } = request.body;
